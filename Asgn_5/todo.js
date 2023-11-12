9d133ebe6c9d18be5fe1ff9f29579c0256eacd4c
Ane Christine Kanestrom <ane.c.kane@gmail.com>
2023-11-12 18:59:44 +0100

    Part3

--- Asgn_5/todo.js
+++ Asgn_5/todo.js
@@ -1,9 +1,28 @@
 let myInput = document.getElementById('myInput')
 let myOutput = document.getElementById('myOutput')
 
+let tasks = [];
+
 function addTask() {
+    if(myInput.value === ''){
+        alert("you must write something!");
+    }  
     
-    myOutput.innerHTML += "<li><input type='checkbox'> "+ myInput.value +"</li>"  
+    else {
 
+    let taskText = myInput.value;
+    let timestamp = new Date().getTime();
 
+    let taskObject = {
+        text: taskText,
+        timestamp: timestamp
+    };
+
+    tasks.push(taskObject);
+
+    myOutput.innerHTML += "<li><input type='checkbox'> " + taskText + "</li>";
+
+    myInput.value = "";
+    }
 }
+
